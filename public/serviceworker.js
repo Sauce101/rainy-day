10f77a43c4a1ef3a7a544cccc20021284016ed4e
Sauce101 <michaelsaucedo443@gmail.com>
2022-03-23 01:49:30 -0700

    added minimal offline page

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html' ];
+const urlsToCache = [ 'index.html', 'offline.html' ];
 
 const self = this;
 
@@ -11,6 +11,17 @@ self.addEventListener('install', (event) => {
                 console.log('Opened cache');
 
                 return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
             })
     )
 });
